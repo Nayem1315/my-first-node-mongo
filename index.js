535457224d6ae5d941c07042d05bcb69464dd21a
Nayem1315 <ndeveloper315@gmail.com>
2021-03-23 22:56:39 +0600

    update

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const express = require('express')
 
 const app = express()
 
+const users = ["Nayem", "Mujahid", "Aminul", "Emon", "Moshiur"]
+
 app.get('/', function (req, res) {
-    res.send('Thank You For Start Me')
+    const fruit = {
+        products: 'Emon adar bapary',
+        name: 'Emon Goru Chor'
+    }
+    res.send(fruit)
+})
+app.get('/fruits/banana', (req, res) => {
+    res.send({ fruit: 'banana', quantity: 1000, price: 10000 })
 })
 
-app.listen(3000, () => console.log('listening to port 3000'))
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query.short);
+    const name = users[id];
+    res.send({ id, name });
+
+})
+
+app.listen(4200, () => console.log('listening to port 4200'))
