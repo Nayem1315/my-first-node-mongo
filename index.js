175c32e2be21329afbc14e09f35cfb6f165d0930
Nayem1315 <ndeveloper315@gmail.com>
2021-03-24 00:59:36 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-
+var cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
 const users = ["Nayem", "Mujahid", "Aminul", "Emon", "Moshiur"]
 
@@ -12,9 +15,6 @@ app.get('/', function (req, res) {
     }
     res.send(fruit)
 })
-app.get('/fruits/banana', (req, res) => {
-    res.send({ fruit: 'banana', quantity: 1000, price: 10000 })
-})
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
@@ -22,6 +22,14 @@ app.get('/users/:id', (req, res) => {
     const name = users[id];
     res.send({ id, name });
 
+})
+
+//post
+app.post('/addUser', (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 10
+    res.send(user)
 })
 
 app.listen(4200, () => console.log('listening to port 4200'))
